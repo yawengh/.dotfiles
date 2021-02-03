9559ea0f46014817f527c8d4e54ef8f580a4cbd6
yawen <None>
2021-02-03 15:47:19 +0800

    first commit

---
+++ tag-zsh/config/zsh/aliases.zsh
@@ -0,0 +1,48 @@
+alias av='source venv/bin/activate'
+alias c='clear'
+alias cdiff='colordiff'
+alias cs='calcurse'
+alias dv='deactivate'
+alias gc='git config credential.helper store'
+alias gg='git clone'
+alias ipy='ipython'
+alias l='ls -la'
+alias lg='lazygit'
+alias ms='mailsync'
+alias mt='neomutt'
+alias r='echo $RANGER_LEVEL'
+alias pu='python3 -m pudb'
+alias ra='ranger'
+# ra() {
+	#if [ -z "$RANGER_LEVEL" ]
+	#then
+		#ranger
+	#else
+		#exit
+	#fi
+#}
+alias s='neofetch'
+alias g='onefetch'
+alias sra='sudo -E ranger'
+alias sudo='sudo -E'
+alias vim='nvim'
+alias gs='git config credential.helper store'
+alias bat='sudo tlp bat'
+alias ac='sudo tlp ac'
+alias gy='git-yolo'
+alias nb='newsboat -r'
+alias nt="sh -c 'cd $(pwd); st' > /dev/null 2>&1 &"
+alias ta='tmux a'
+alias t='tmux'
+alias lo='lsof -p $(fps) +w'
+
+#  terminal alias
+alias hp='export http_proxy="localhost:1081" 
+	export https_proxy="localhost:1081" 
+	echo "http_proxy:"$http_proxy "https_proxy:"$https_proxy'
+
+alias np='export http_proxy="" export https_proxy="" 
+	echo "http_proxy:"$http_proxy "https_proxy:"$https_proxy'
+
+#  firetool
+alias firetool="sudo /opt/fireTools/run.sh"
